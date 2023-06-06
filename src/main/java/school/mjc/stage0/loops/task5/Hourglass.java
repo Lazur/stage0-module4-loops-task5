88279db7dec2715bfa88cc1b4c98810c59c6cf88
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-06-06 23:48:25 +0300

    Module 4 task5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,36 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int counter = height;
+         // Upper half of the hourglass
+         for (int i = 0; i < height / 2; i++) {
+            printLine(i, counter);
+            counter -=2;
+        }
 
+        // Lower half of the hourglass
+        for (int i = height / 2; i >= 0; i--) {
+            printLine(i, counter);
+            counter +=2;
+        }
+    }
+
+    private static void printLine(int spaces, int eights) {
+        if (eights == 0) {
+            return;
+        }
+        for (int i = 0; i < spaces; i++) {
+            System.out.print(" ");
+        }
+
+        for (int i = 0; i < eights; i++) {
+            System.out.print("8");
+        }
+
+        for (int i = 0; i < spaces; i++) {
+            System.out.print(" ");
+        }
+
+        System.out.println("");
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,29 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
+        if (length == 0 && height == 0) {
+            System.out.print("");
+            return;
+        }
+        // Print the top side of the rectangle
+        for (int i = 0; i < length; i++) {
+            System.out.print("8");
+        }
+        System.out.println();
 
+        // Print the middle part of the rectangle
+        for (int i = 0; i < height - 2; i++) {
+            System.out.print("8");
+            for (int j = 0; j < length - 2; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("8");
+        }
+
+        // Print the bottom side of the rectangle
+        for (int i = 0; i < length; i++) {
+            System.out.print("8");
+        }
+        System.out.println();
     }
 }
